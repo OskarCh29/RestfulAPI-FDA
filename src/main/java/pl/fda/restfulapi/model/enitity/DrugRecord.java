9182e5ac532c7bb4ec6ffe4a260eb005880aab70
OskarCh29 <oskarcharytoniuk29@gmail.com>
2025-04-25 09:55:08 +0200

    Unnecessary fields removed

--- src/main/java/pl/fda/restfulapi/model/enitity/DrugRecord.java
+++ src/main/java/pl/fda/restfulapi/model/enitity/DrugRecord.java
@@ -19,22 +19,18 @@ import lombok.*;
 public class DrugRecord {
     @Id
     @Column(name = "application_number")
-    @NotNull(message = "Application number was not provided")
     @NotBlank(message = "Application number empty")
     private String applicationNumber;
 
     @Column(name = "manufacturer_name")
-    @NotNull(message = "Manufacturer name was not provided")
     @NotBlank(message = "Manufacturer field empty")
     private String manufacturerName;
 
     @Column(name = "substance_name")
-    @NotNull(message = "Substance name was not provided")
     @NotBlank(message = "Substance name missing")
     private String substanceName;
 
     @Column(name = "product_number")
-    @NotNull(message = "Product numbers was not provided")
     @NotBlank(message = "Product number missing")
     @Pattern(regexp = "^[0-9]{3,}")
     private String productNumber;
